c02fc63ee658c236b7f516ab716d9c6dff53c36e
unknown <u18058737@tuks.co.za>
2020-08-27 20:14:07 +0200

    Final Cursor Design with highlight

--- js/cursor/follow.js
+++ js/cursor/follow.js
@@ -12,21 +12,18 @@ function followMouse(e){
   highlight.css({left: e.clientX,top:e.clientY });
 }
 function growCursor(){
-  cursor.css("opacity","50%");
+  cursor.css("opacity","0%");
   cursor.css("width","13%");
+
+  highlight.css("width","13%");
   highlight.css("opacity","100%");
-  highlight.css("transition-delay","1s");
+
 
 }
 function shrinkCursor(){
-  // highlight.css("opacity","0%");
-  // cursor.css("opacity","100%");
-  // highlight.css("width","8%");
 
   cursor.css("opacity","100%");
   cursor.css("width","8%");
   highlight.css("opacity","0%");
-
-  highlight.css("opacity","0%");
-  highlight.css("transition-delay","0s");
+  highlight.css("width","8%");
 }
