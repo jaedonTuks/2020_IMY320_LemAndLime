1455588c5c0c0ff7a5968220935c6c93ebcdffbe
Jonathan Bertram <54099984+u18138358@users.noreply.github.com>
2020-11-03 09:30:42 +0200

    Removed commented out code that I forgot to take out before merging

--- js/SinglePages/movie.js
+++ js/SinglePages/movie.js
@@ -4,9 +4,3 @@ function ended(){
   console.log("vid fin 1");
   document.getElementById('watch-now').style.display = "block";
 }
-
-/*var shortFilm = document.getElementById("shortFilm");
-shortFilm.onended = function() {
-    console.log("vid fin 2");
-    document.getElementById('watch-now').hidden = false;
-};*/
